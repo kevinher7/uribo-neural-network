94dada6f37d1b89d1d0c4a9fc3436867722248c1
yomawarikotoba <xiangyinbencheng@gmail.com>
2025-04-28 14:34:10 +0900

    refactor: forward()methodをclassの一番上に持ってくることを解決

--- src/model/neuron.py
+++ src/model/neuron.py
@@ -7,6 +7,12 @@ class Neuron:
         self.weight = np.random.randn(output_dim, input_dim)
         self.bias = np.random.randn(output_dim)
 
+    def forward(self, x):
+        # ユニット入力→活性化→活性化関数→ユニット出力の流れを総合して行う
+        a = self._make_activations(x)
+        y = self._activation_function(a)
+        return y
+
     def _make_activations(self, x):
         # forward()は一つのunitに関してinputからoutputを計算するmethod
         a = np.dot(self.weight, x) + self.bias
@@ -15,10 +21,4 @@ class Neuron:
     def _activation_function(self, a):
         # 今回はtanhを使用します
         y = np.tanh(a)  # (e^x - e^(-x))/(e^x + e^(-x))
-        return y
-
-    def forward(self, x):
-        # ユニット入力→活性化→活性化関数→ユニット出力の流れを総合して行う
-        a = self._make_activations(x)
-        y = self._activation_function(a)
         return y
