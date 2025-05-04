b3dee90d063c8e24c967407d09343a266699f4ce
Kevin <kevinhernem@gmail.com>
2025-05-05 00:29:18 +0900

    chore: add typing and docstrings

--- src/model/nn.py
+++ src/model/nn.py
@@ -1,14 +1,33 @@
+import numpy as np
+from numpy.typing import NDArray
+
 from src.model.layer import Layer
 
 
 class NeuralNetwork:
-    def __init__(self, layers: list[Layer]) -> None:
-        self.layers = layers
+    def __init__(self, input_dim: int, layers: list[Layer]) -> None:
+        """Initialize NN by assigning input dimensions to all Layers"""
+        # Remove the first layer (hidden) and append it to NN
+        first_hidden_layer = layers.pop(0)
+        first_hidden_layer.input_dim = input_dim
+        self.layers = [first_hidden_layer]
 
-    def forward(self, data: list[int], classification: bool = False) -> None:
+        for index, layer in enumerate(layers):
+            # Give input dimension as the previous layer's number of neurons
+            layer.input_dim = self.layers[index].num_neuron
+            self.layers.append(layer)
+
+    def forward(self, data: NDArray[np.float64], *, classification: bool = False) -> float:
         """
         Forward method for layer class.
-        It takes a one dimensional numpy array as input.
+        Args:
+            data (NDArray[np.float64]): 1 dimmensional numpy array
+
+        Kwargs:
+            classification (bool): determines whether to use regression or classification
+
+        Returns:
+            output (float): for regression
         """
         layer_output = data
         for layer in self.layers:
