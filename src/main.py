23fd414360667689d28aceb670d6a89fdc3caecd
Kevin <kevinhernem@gmail.com>
2025-05-05 01:22:47 +0900

    feat: implement forward passing of uribo neural network!

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 import click
+import numpy as np
 
 from src.model.layer import Layer
 from src.model.nn import NeuralNetwork
@@ -7,8 +8,10 @@ from src.model.nn import NeuralNetwork
 @click.command()
 def console():
     """Entry point to access the Neural Network"""
+    x_data = np.array([1])
+
     uribo_neural_network = NeuralNetwork(
-        input_dim=3,
+        input_dim=x_data.size,
         layers=[
             Layer(3),
             Layer(1),
@@ -16,6 +19,7 @@ def console():
     )
 
     print(f"I am Uribo! This is me: {uribo_neural_network}")
+    print(f"This is my output with for x={x_data}: {uribo_neural_network.forward(x_data)}")
 
 
 if __name__ == "__main__":
