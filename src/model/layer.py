5cc443024265adad3474967b11011fbd68c077ae
kaxon_i_in_x <xiangyinbencheng@gmail.com>
2025-05-05 00:41:45 +0900

    feat:make layer.py and fix neuron.py and add annotations #4
    * feat:make layer.py (in progress)
    
    * fix:fix layer.py(in progress)
    
    * fix:fix neuron.py content from "y (in forward)" to "float(y)" ,and layer.py content about "__init__" and "forward".
    
    * chore:annotationを追加
    
    * chore:annotationが欠けていた部分の補修とlist in floatのannotationを便利に行うためにNDArrayをimport
    
    * chore:Layerのinput_dimにもannotationを追加
    
    * chore:neuron.pyとlayer.pyのannotationを追加

--- src/model/layer.py
+++ src/model/layer.py
@@ -1,4 +1,18 @@
+from neuron import Neuron
+import numpy as np
+from numpy.typing import NDArray
+
+
 class Layer:
-    def __init__(self) -> None:
-        print("I am a Layer")
-        pass
+    def __init__(self, num_neuron: int, input_dim: int) -> None:
+        self.neurons = []
+        # neuronsリストにnum_nueronで指定した個数分ユニットを追加
+        for index in range(num_neuron):
+            self.neurons.append(Neuron(input_dim))
+
+    def forward(self, data: NDArray[np.float64]) -> NDArray[np.float64]:
+        self.outputs = []
+        # neuronsリストに追加されたユニットを順に実行し、それぞれの結果をoutputsリストに追加
+        for neuron in self.neurons:
+            self.outputs.append(neuron.forward(data))
+        return np.array(self.outputs)
--- src/model/neuron.py
+++ src/model/neuron.py
@@ -1,25 +1,26 @@
 import numpy as np
+from numpy.typing import NDArray
 
 
 class Neuron:
-    def __init__(self, input_dim, output_dim=1):
+    def __init__(self, input_dim: int, output_dim=1) -> None:
         # TODO いずれこのweightとbiasは変更する必要がありそう（ローカル最小に引っかかったり学習速度の関係があるから）
         # TODO 重みベクトルはベクトルに限らず行列になる可能性（output_dim=>2のとき）があるため、それは後で検討する
         self.weight = np.random.randn(input_dim)
         self.bias = np.random.randn(output_dim)
 
-    def forward(self, x):
+    def forward(self, x: NDArray[np.float64]) -> NDArray[np.float64]:
         # ユニット入力→活性化→活性化関数→ユニット出力の流れを総合して行う
         a = self._make_activations(x)
         y = self._activation_function(a)
-        return y
+        return float(y)
 
-    def _make_activations(self, x):
+    def _make_activations(self, x: NDArray[np.float64]) -> NDArray[np.float64]:
         # forward()は一つのunitに関してinputからoutputを計算するmethod
         a = np.dot(self.weight, x) + self.bias
         return a
 
-    def _activation_function(self, a):
+    def _activation_function(self, a: float) -> float:
         # 今回はtanhを使用します
         y = np.tanh(a)  # (e^x - e^(-x))/(e^x + e^(-x))
         return y
