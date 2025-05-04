3971fd76b454b598dedea4f3012bfc68c8fc8cad
Kevin <kevinhernem@gmail.com>
2025-05-04 23:58:33 +0900

    feat: create init and forward methos in nn class

--- src/model/nn.py
+++ src/model/nn.py
@@ -1,4 +1,27 @@
+from src.model.layer import Layer
+
+
 class NeuralNetwork:
-    def __init__(self) -> None:
-        print("I am a Neural Network")
-        pass
+    def __init__(self, layers: list[Layer]) -> None:
+        self.layers = layers
+
+    def forward(self, data: list[int], classification: bool = False) -> None:
+        """
+        Forward method for layer class.
+        It takes a one dimensional numpy array as input.
+        """
+        layer_output = data
+        for layer in self.layers:
+            layer_output = layer.forward(layer_output)
+
+        if classification:
+            output = self._softmax(layer_output)
+        else:
+            output = layer_output
+
+        return output
+
+    def _softmax() -> None:
+        raise NotImplementedError(
+            "Neural Network does not support classification (only regression)"
+        )
