aff82f4d4d8bd216f974469fb13e946fb2d0e9a7
yomawarikotoba <xiangyinbencheng@gmail.com>
2025-05-04 23:46:05 +0900

    fix:fix layer.py(in progress)

--- src/model/layer.py
+++ src/model/layer.py
@@ -2,14 +2,12 @@ from src.model.neuron import Neuron
 
 
 class Layer:
-    def __init__(self, num_neuron: int, input_dim, output_dim) -> None:
+    def __init__(self, num_neuron: int, input_dim) -> None:
         self.neurons = []
         for index in num_neuron:
-            self.neurons.append(Neuron(input_dim, output_dim))
+            self.neurons.append(Neuron(input_dim))
 
     def forward(self, data):
-        self.outputs = np
+        self.outputs = []
         for neuron in self.neurons:
             self.outputs.append(neuron.forward(data))
-
-        # matomete
