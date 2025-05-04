57ab4e99bdcfb233e51d6f0d3e5a67c91bd1494a
yomawarikotoba <xiangyinbencheng@gmail.com>
2025-05-04 23:36:25 +0900

    feat:make layer.py (in progress)

--- src/model/layer.py
+++ src/model/layer.py
@@ -1,4 +1,15 @@
+from src.model.neuron import Neuron
+
+
 class Layer:
-    def __init__(self) -> None:
-        print("I am a Layer")
-        pass
+    def __init__(self, num_neuron: int, input_dim, output_dim) -> None:
+        self.neurons = []
+        for index in num_neuron:
+            self.neurons.append(Neuron(input_dim, output_dim))
+
+    def forward(self, data):
+        self.outputs = np
+        for neuron in self.neurons:
+            self.outputs.append(neuron.forward(data))
+
+        # matomete
