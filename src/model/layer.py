522d07aa853bbc0e32b5eb0f230e2d6ff23ead7a
Kevin <kevinhernem@gmail.com>
2025-06-13 17:42:07 +0900

    feat: store weights in layer class

--- src/model/layer.py
+++ src/model/layer.py
@@ -6,8 +6,9 @@ from numpy.typing import NDArray
 class Layer:
     def __init__(self, num_neurons: int, *, output_layer: bool = False) -> None:
         self.num_neurons = num_neurons
+        self.neurons: list[Neuron] = [Neuron() for _index in range(num_neurons)]
+
         self.input_dim = 0
-        self.neurons = []
         self.output_layer = output_layer
 
     def forward(self, data: NDArray[np.float64]) -> NDArray[np.float64]:
@@ -19,13 +20,24 @@ class Layer:
 
         return np.array(self.outputs)
 
+    def backward(
+        self,
+        delta_next_layer: NDArray[np.float64],
+        weights_to_next_layer: NDArray[np.float64],
+    ) -> NDArray[np.float64]:
+        for i in range(len(self.neurons) - 1, 0, -1):
+            self.neurons[i].backward(delta_next_layer, weights_to_next_layer)
+
     def build(self) -> None:
-        # neuronsリストにnum_nueronで指定した個数分ユニットを追加
+        self.weights = np.random.randn(self.input_dim, self.num_neurons)
+
         if self.output_layer:
             for _index in range(self.num_neurons):
                 self.neurons.append(Neuron(self.input_dim))
 
-            return
+        self.weights = [neuron.weight for neuron in self.neurons]
+
+        return
 
         # Add "bias neuron" to neuron list
         self.neurons.append(Neuron(self.input_dim, bias_neuron=True))
