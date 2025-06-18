9b504a0e2f68486ec998df7d6b69b5dea8c293c5
Kevin <kevinhernem@gmail.com>
2025-06-19 00:53:04 +0900

    chore: delete unused backward method in layer

--- src/model/layer.py
+++ src/model/layer.py
@@ -26,40 +26,6 @@ class Layer:
 
         return self.outputs
 
-    def backward(
-        self,
-        deltas_next_layer: NDArray[np.float64],
-        weights_to_next_layer: NDArray[np.float64],
-    ) -> NDArray[np.float64]:
-        # Calculate the gradient
-        # grad_j = d_k*z_j
-        self.grad = np.outer(deltas_next_layer, self.outputs.T)
-        # grad E_kj
-        # Row: k (next layer unit)
-        # Column: j (current layer unit)
-
-        # Calculate this layer's delta
-        if self.augmentation:
-            # Remove the bias related parameters (bias delta is not needed)
-            # since no units send connections to it
-            # (so this delta is not involved gradient calculations)
-            self.outputs = self.outputs[:-1]
-            weights_to_next_layer = weights_to_next_layer[:-1]
-
-        # Since we use hyperbolic tangent as the activation function
-        # h(x)  = tanh(x)
-        # h'(x) = 1 - h(x)^2
-        activation_func_derivatives = np.array(1 - np.square(self.outputs))
-
-        # Calculate this layer's deltas (errors)
-        # delta_j = h'(a_j)* sum(w_kj*d_k)
-        # .T to transpose the weight matrix
-        sum = np.matmul(deltas_next_layer, weights_to_next_layer.T)
-
-        layer_deltas = activation_func_derivatives * sum
-
-        return layer_deltas
-
     def build(self, input_dim, augmentation: bool) -> Self:
         """
         Creates connections with previous layer as a weight array
